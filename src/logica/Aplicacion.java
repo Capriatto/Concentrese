373482bf68888dd6d223824e91d9c60ade2f0dc9
Jose_sebastian <michu-1@hotmail.com>
2015-09-23 16:42:13 -0500

    primer commit

---
+++ src/logica/Aplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ * Esta clase inicializa el proyecto.
+ *
+ * @author coder
+ * @version 1.0
+ */
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ui.MainInterface().setVisible(true);
+    }
+    
+}
---
+++ src/logica/Jugador.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author coder
+ */
+public class Jugador {
+    private int id;
+    private String nombre;
+    private double tiempo;
+    private int jugadas;
+
+    public Jugador(int id, String nombre, double tiempo, int jugadas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tiempo = 0.0;
+        this.jugadas = 0;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public int getJugadas() {
+        return jugadas;
+    }
+
+    public void setJugadas(int jugadas) {
+        this.jugadas = jugadas;
+    }
+    
+}
